3064eb7dd573ffae839821e0dd601336109067e2
Vishal Goswami <vg0767875@gmail.com>
2026-04-29 10:26:15 +0530

    Initial commit

---
+++ controllers/authController.js
@@ -0,0 +1,33 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hash
+  });
+
+  res.json(user);
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(400).json("User not found");
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) return res.status(400).json("Wrong Password");
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+  res.json({ token });
+};
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
+app.listen(process.env.PORT, () => {
+  console.log("Server Running");
+});
